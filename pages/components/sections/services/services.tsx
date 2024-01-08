fea6e25edaac48d65c9961e8baa2a54edab0be76
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-01-08 09:54:34 +0200

    Updated services section

--- pages/components/sections/services/services.tsx
+++ pages/components/sections/services/services.tsx
@@ -1,12 +1,14 @@
 import Title from '@/components/title';
 import useResponsive from '@/hooks/useResponsive';
-import { Box, Container, Stack } from '@mui/material';
+import { Box, Container, Stack, Typography } from '@mui/material';
 import { motion } from 'framer-motion';
+import useTranslation from 'next-translate/useTranslation';
 import Image from 'next/image';
 
 type Props = {};
 
 const ServicesSection = (props: Props) => {
+	const { t } = useTranslation();
 	const smUp = useResponsive({ key: 'sm', query: 'up' });
 
 	return (
@@ -27,7 +29,29 @@ const ServicesSection = (props: Props) => {
 				<Stack justifyContent={'center'} alignItems={'center'} spacing={4} mt={4} useFlexGap>
 					<Title title="home:services_section_title" subtitle="home:services_section_subtitle" />
 					<Box position={'relative'} width={{ xs: 1, md: 700 }} height={{ xs: 300, md: 500 }}>
-						<Image src={`/images/services_hero_${smUp ? 'desktop' : 'mobile'}.png`} alt={'Laundry Dryer'} style={{ objectFit: 'contain' }} quality={100} fill priority />
+						<Image src={`/images/services_hero.png`} alt={'Laundry Dryer'} style={{ objectFit: 'contain' }} quality={100} fill priority />
+						<Stack
+							position={'absolute'}
+							bottom={'20%'}
+							left={{ xs: '0%', sm: '75%' }}
+							direction={'row'}
+							alignItems={'center'}
+							bgcolor={'primary.main'}
+							p={{ xs: 1, sm: 4 }}
+							borderRadius={4}
+							spacing={2}
+							sx={{ transform: { xs: 'translate(0%, 10%)', sm: 'translate(0%, -20%)' } }}
+							useFlexGap>
+							<Image src={'/svgs/services_badge.svg'} alt={'Laundry Dryer'} width={48} height={48} quality={100} />
+							<Stack color={'common.white'}>
+								<Typography variant={smUp ? 'h5' : 'body1'} fontWeight={'bold'}>
+									12,753 +
+								</Typography>
+								<Typography variant={smUp ? 'body1' : 'body2'} maxWidth={{ xs: '10ch', sm: '20ch' }} whiteSpace={{ sm: 'nowrap' }}>
+									{t('home:service_section_customer_trusted')}
+								</Typography>
+							</Stack>
+						</Stack>
 					</Box>
 				</Stack>
 			</Container>
