c577ccba348fb6c08c320a16d3452a089b542c14
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-02-19 13:43:29 +0200

    Updated Meta Pixel script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,7 +30,7 @@ export default function Document({ emotionStyleTags, ...props }: Props) {
 					t.src=v;s=b.getElementsByTagName(e)[0];
 					s.parentNode.insertBefore(t,s)}(window, document,'script',
 					'https://connect.facebook.net/en_US/fbevents.js');
-					fbq('init', '7189379744474967');
+					fbq('init', '659317889432179');
 					fbq('track', 'PageView');
 				`}</Script>
 				<link rel="manifest" href="/manifest.json" />
